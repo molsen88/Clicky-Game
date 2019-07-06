4c34bc015622cf25476cfee9894b78764eb340da
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-07-05 21:04:09 -0600

    Updates

---
+++ precache-manifest.951119bca880d4734a97d8e113f97de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a8c9101855ce38308de8ca0dcb1b1a",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "b03d64354f4b8891db4f",
+    "url": "/Clicky-Game/static/css/main.7b85409d.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "b03d64354f4b8891db4f",
+    "url": "/Clicky-Game/static/js/main.1263d930.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
